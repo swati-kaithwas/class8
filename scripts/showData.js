30fad12b3fde88f0b10cdd057259589d81affde7
swati <91478967+swati-kaithwas@users.noreply.github.com>
2021-10-19 19:21:17 +0530

    class 8

---
+++ scripts/showData.js
@@ -0,0 +1,31 @@
+//getdata()
+//append()
+
+//getdata()
+async function getData(url){
+
+    let res=await fetch(url)
+    let data=await res.json()
+
+    return data
+}
+
+
+//append()
+
+function append(data,container) {
+
+    data.forEach(({title,image}) => {
+ let div=document.createElement("div");
+ let p=document.createElement("p");
+
+ p.innerText=title;
+ let img=document.createElement("img");
+ img.src=image;
+div.append(img,p);
+container.append(div);
+    })
+}
+
+
+export{ getData , append }
